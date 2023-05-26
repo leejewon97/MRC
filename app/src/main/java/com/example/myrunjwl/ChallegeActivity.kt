b1db6fcf2e58a5536fef42b0034ef4bf892d16b8
Jtility <leejewon97@gmail.com>
2023-05-27 02:00:04 +0900

    init 최소한의 xml과 앞으로 할 내용 주석

---
+++ app/src/main/java/com/example/myrunjwl/ChallegeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myrunjwl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChallegeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challege)
+        // select -> check by dialog -> change color
+        // write txt now challenge at first line
+        // txt will written all challenges -> have to read and show
+    }
+}
---
+++ app/src/main/java/com/example/myrunjwl/SettingActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myrunjwl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+        // txt
+        // voice : 0 | 1
+        // ap : 0 | 5 | 10 | 30
+        // cd : 0 | 3 | 5 | 10
+        // broadcast : rotate
+    }
+}
